1175be3555363e2cf6808bef01c59de2826f036f
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-10-04 21:13:17 +0300

    feat: Add networking to AccountSummary

---
+++ BankAccount/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  BankAccount
+//
+//  Created by Sergei Poluboiarinov on 03.10.2022.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
